92ffa8f4ca809d8316fc46d4dc3660f4e7b843fa
jayshah2005 <jayshah3616@gmail.com>
2026-02-05 10:30:26 -0500

    fix change resources enum to a class

---
+++ PlayerAccount/Resources.java
@@ -0,0 +1,13 @@
+package PlayerAccount;
+public class Resources {
+
+    int wood;
+    int gold;
+    int iron;
+
+    Resources(int wood, int gold, int iron) {
+        this.wood = wood;
+        this.gold = gold;
+        this.iron = iron;
+    }
+}
