eaebd884dc03d2b9a47aec415aacedd37d380ad7
jayshah2005 <jayshah3616@gmail.com>
2026-03-04 12:46:07 -0500

    chore: add constructor for arbiter class

--- src/Utility/Arbitrer.java
+++ src/Utility/Arbitrer.java
@@ -1,6 +1,21 @@
 package src.Utility;
 
+import src.GUI.GUI;
+
 public class Arbitrer {
+
+    float attackScore;
+    float defenseScore;
+
+    Arbitrer() {
+        attackScore = 0;
+        defenseScore = 0;
+    }
+
+    Arbitrer(float attackScore, float defenseScore) {
+        this.attackScore = attackScore;
+        this.defenseScore = defenseScore;
+    }
 
     public AttackOutcome judgeAttack(float AttackScore, float DefenceScore){
         boolean success = AttackScore > DefenceScore;
