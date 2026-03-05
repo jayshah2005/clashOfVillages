44b0e98f2cc016a67a039f7dd6600f4a549674cf
jayshah2005 <jayshah3616@gmail.com>
2026-03-04 23:41:37 -0500

    feat: Fighter enum linked to give Fighter constructor

--- src/enums/Fighters.java
+++ src/enums/Fighters.java
@@ -1,8 +1,31 @@
 package src.enums;
 
+import src.PlayerAccount.Units.*;
+
 public enum Fighters {
-    ARCHERS,
-    CATAPULT,
-    KNIGHT,
-    SOLDIER
+    ARCHER("Archer"),
+    CATAPULT("Catapult"),
+    KNIGHT("Knight"),
+    SOLDIER("Soldier");
+
+    public final String label;
+
+    Fighters(String label){
+        this.label = label;
+    }
+
+    public static Fighter flighterFromLabel(String label){
+        switch (label){
+            case "archers":
+                return new Archer();
+            case "catapult":
+                return new Catapult();
+            case "knight":
+                return new Knight();
+            case "soldier":
+                return new Soldiers();
+            default:
+                return null;
+        }
+    }
 }
