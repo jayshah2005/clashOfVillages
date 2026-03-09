6054436a4f634a5e29a24b9785f3d9b1fd43ac75
jayshah2005 <jayshah3616@gmail.com>
2026-03-08 22:30:13 -0400

    fix: remove enum descriptors

--- src/enums/View.java
+++ src/enums/View.java
@@ -4,10 +4,10 @@ package src.enums;
  * Enum representing all possible game views/screens.
  */
 public enum View {
-    VILLAGE, // Main village view
-    SHOP,    // Shop view
-    ATTACK,  // Attack screen
-    TRAIN,   // Training screen
-    GATHER,  // Resource gathering screen
-    UPGRADE  // Upgrade screen
+    VILLAGE,
+    SHOP,
+    ATTACK,
+    TRAIN,
+    GATHER,
+    UPGRADE
 }
